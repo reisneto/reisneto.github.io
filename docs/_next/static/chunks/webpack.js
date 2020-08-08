72567d266389cce5f99a07a6a15047f517e456d7
Alvaro Reis <reisneto.alvaro@gmail.com>
2020-08-08 00:07:15 -0400

    export(fix): re add .nojekyll

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6d4cfe0b80ef6485854d";
+/******/ 	var hotCurrentHash = "064d3e0c9ce797bd3bfc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
