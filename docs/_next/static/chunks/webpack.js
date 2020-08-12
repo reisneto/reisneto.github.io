3ab1b7df26097c0c1ec6fd5be29bc173ba6de211
Alvaro Reis <reisneto.alvaro@gmail.com>
2020-08-12 08:26:06 -0400

    chore: new post

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "064d3e0c9ce797bd3bfc";
+/******/ 	var hotCurrentHash = "b8b63b400f1614d69c39";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
